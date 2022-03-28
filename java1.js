e7a479135dc92bc235a1fc632912c2d7c59bcee9
paul1mensah <93052111+paul1mensah@users.noreply.github.com>
2022-03-28 03:29:21 +0000

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,21 @@
+function add(){
+    let x=5;
+    let y=10;
+    var z=x+y;
+    document.write(z);
+}
+function display(){
+    window.print();
+}
+function warn(){
+    window.alert("welcome osimasi");
+}
+function inside(){
+    document.getElementById(demo).innerHTML="Come back to me ooo wifee. Come back to me";
+}
+function change1(){
+    document.getElementById('change1').src='eldad.jpg';
+}
+function change2(){
+    document.getElementById('change1').src='wife.jpg';
+}
